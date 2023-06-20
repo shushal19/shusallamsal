d06016715dcef8a4fe47e340550cf07893db6393
Shusal Lamsal <shusallamsal@gmail.com>
2023-06-20 21:04:12 +0545

    new
    update

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Code to handle form submission and email sending goes here
+}
+?>
--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Smooth scrolling for anchor links
-document.addEventListener('DOMContentLoaded', function() {
-    const anchorLinks = document.querySelectorAll('a[href^="#"]');
-    for (let anchorLink of anchorLinks) {
-      anchorLink.addEventListener('click', function(e) {
-        e.preventDefault();
-  
-        const targetId = anchorLink.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-  
-        if (targetElement) {
-          window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-          });
-        }
-      });
-    }
-  });
-  
+// Image glazing effect
+window.addEventListener('scroll', () => {
+    const image = document.querySelector('section img');
+    const scrollPosition = window.scrollY;
+
+    image.style.transform = `translateY(-${scrollPosition * 0.5}px)`;
+});
