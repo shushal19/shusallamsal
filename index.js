f3df071c50f7737956d4a4705f60a9fdf2ec4b41
Shusal Lamsal <59756277+shushal19@users.noreply.github.com>
2023-06-20 21:19:02 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-// Fade in sections when scrolled into view
-document.addEventListener("DOMContentLoaded", function() {
-    const sections = document.querySelectorAll("section");
+new WOW().init();
 
-    const fadeIn = function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                entry.target.style.opacity = "1";
-                entry.target.style.transform = "translateY(0)";
-            }
-        });
-    };
 
-    const sectionObserver = new IntersectionObserver(fadeIn, {
-        root: null,
-        threshold: 0.3
-    });
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
 
-    sections.forEach(function(section) {
-        section.style.opacity = "0";
-        section.style.transform = "translateY(50px)";
-        sectionObserver.observe(section);
-    });
+    if (scroll >= 300) {
+        $(".fixed-top").addClass("change-fixed-top");
+    } else {
+        $(".fixed-top").removeClass("change-fixed-top");
+    }
+});
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1250);
+    $('html, body').animate({path : new $.path.bezier(bezier_params)})
+
 });
