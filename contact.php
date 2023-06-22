fa686617814c4ab41cd4259636a87a1f3a27cb34
Shusal Lamsal <shusallamsal@gmail.com>
2023-06-22 08:16:06 +0545

    jhhj

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-
-    // Code to handle form submission and email sending goes here
-}
-?>
--- script.js
+++
@@ -1,7 +0,0 @@
-// Image glazing effect
-window.addEventListener('scroll', () => {
-    const image = document.querySelector('section img');
-    const scrollPosition = window.scrollY;
-
-    image.style.transform = `translateY(-${scrollPosition * 0.5}px)`;
-});
